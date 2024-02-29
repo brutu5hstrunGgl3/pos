8ff309d1e6e32c3c9c91f70636c23ca1fdb6a4b8
Rizkiputraramadhan <rizkikuliit@gmail.com>
2024-02-29 15:58:31 +0700

    seeder admin

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -12,17 +12,18 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(100)->create();
+        //User::factory(100)->create();
 
         User::create([
             'name' => 'Fadli Admin',
             'email' => 'Fadli@admin.com',
             'email_verified_at' => now(),
+            'roles' => 'ADMIN',
             'password' => Hash::make('12345678'),
             'phone' => '08989898989',
-            
+
         ]);
 
-       
+
     }
 }
