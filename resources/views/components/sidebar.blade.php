9e2c2fc28f79372d7c0c8e1b16233a17614d67ca
Brutu5h5trungGl3 <45219933+brutu5hstrunGgl3@users.noreply.github.com>
2024-03-15 10:06:24 +0700

    sidebar test

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,7 +1,7 @@
 <div class="main-sidebar sidebar-style-2">
     <aside id="sidebar-wrapper">
         <div class="sidebar-brand">
-            <a href="index.html">POS KOPERASI UPS</a>
+            <a href="index.html">PERPUSTAKAAN PSU</a>
         </div>
         <div class="sidebar-brand sidebar-brand-sm">
             <a href="index.html">KOPERASI UPS</a>
@@ -12,41 +12,80 @@
                 <a href="#" class="nav-link has-dropdown"> <i class="fas fa-fire"></i><span>Home</span></a>
                 <ul class="dropdown-menu">
                     <li>
-                        <a class="nav-link" href="{{route('home') }}">Berita & Albums</a>
+                        <a class="nav-link" href="{{ route('home') }}">Berita & Albums</a>
                     </li>
 
                 </ul>
             </li>
-            
-            @if (auth()->user()->roles == 'ADMIN')
-            <li class="nav-item dropdown ">
-                <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Users</span></a>
-                <ul class="dropdown-menu">
-                    <li>
-                        <a class="nav-link" href="{{route('user.index')}}">User List</a>
-                    </li>
 
-                </ul>
-            </li>
+            @if(auth()->user()->roles == 'ADMIN')
+                <li class="nav-item dropdown ">
+                    <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Users</span></a>
+                    <ul class="dropdown-menu">
+                        <li>
+                            <a class="nav-link" href="{{ route('user.index') }}">User List</a>
+                        </li>
+
+                    </ul>
+                </li>
+            @endif
+            @if(auth()->user()->roles == 'ADMIN')
+                <li class="nav-item dropdown ">
+                    <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Stock Buku</span></a>
+                    <ul class="dropdown-menu">
+                        <li>
+                            <a class="nav-link" href="{{ route('product.index') }}">Tambah Buku</a>
+                        </li>
+                    </ul>
             @endif
 
-              <li class="nav-item dropdown ">
-                <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Product</span></a>
-                <ul class="dropdown-menu">
-                    <li>
-                        <a class="nav-link" href="{{ route('product.index')}}">Product</a>
-                    </li>
-                </ul>
-
+            @if(auth()->user()->roles == 'STAFF')
                 <li class="nav-item dropdown ">
-                <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Schedules</span></a>
-                <ul class="dropdown-menu">
-                    <li>
-                        <a class="nav-link" href="">Schedules List</a>
-                    </li>
-                </ul>
-            </li>
+                    <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Stock Buku</span></a>
+                    <ul class="dropdown-menu">
+                        <li>
+                            <a class="nav-link" href="{{ route('product.index') }}">Tambah Buku</a>
+                        </li>
+                    </ul>
+            @endif
 
+            @if(auth()->user()->roles == 'ADMIN')
+                <li class="nav-item dropdown ">
+                    <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Pengunjung</span></a>
+                    <ul class="dropdown-menu">
+                        <li>
+                            <a class="nav-link" href="">Daftar Tamu</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown ">
+                    <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Peminjam Buku</span></a>
+                    <ul class="dropdown-menu">
+                        <li>
+                            <a class="nav-link" href="">Anggota peminjam</a>
+                        </li>
+                    </ul>
+                <li class="nav-item dropdown ">
+                    <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Pengembalian
+                            Buku</span></a>
+                    <ul class="dropdown-menu">
+                        <li>
+                            <a class="nav-link" href="">Pengembalian</a>
+                        </li>
+            @endif
+            @if(auth()->user()->roles == 'USER')
+                <ul class="sidebar-menu">
+                    <li class="nav-item dropdown ">
+                        <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Peminjaman
+                                Buku</span></a>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a class="nav-link" href="">Peminjaman buku</a>
+                            </li>
+            @endif
 
     </aside>
+
+
+
 </div>
